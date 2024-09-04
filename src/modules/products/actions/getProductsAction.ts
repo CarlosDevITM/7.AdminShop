ac3ed2a31ea198bd2da766b43cfebfc541049fa8
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-04 15:10:57 -0600

    Se creó una función para traer las imágenes con un endpoint comleto

--- src/modules/products/actions/getProductsAction.ts
+++ src/modules/products/actions/getProductsAction.ts
@@ -1,5 +1,6 @@
 import { adminApi } from '@/api/adminApi';
 import type { IAllProducts } from '../interfaces/IAllProducts';
+import { getProductImageAction } from './getProductsImageAction';
 
 //Obtener productos.
 export const getProductsAction = async (actualPage: number = 1, productsLimit: number = 10) => {
@@ -10,8 +11,11 @@ export const getProductsAction = async (actualPage: number = 1, productsLimit: n
     const { data } = await adminApi.get<IAllProducts[]>(
       `/products?limit=${productsLimit}&offset=${actualPage * productsLimit}`,
     );
-    console.log(data);
-    return data;
+    //Return the images
+    return data.map((product) => ({
+      ...product,
+      images: product.images.map(getProductImageAction),
+    }));
   } catch (error) {
     console.log(error);
     throw new Error('Error loading products');
