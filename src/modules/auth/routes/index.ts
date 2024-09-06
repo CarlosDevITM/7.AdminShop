9b5b8ed72a515bb68f2c2bf856e60b3e89ba1b35
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-05 18:26:02 -0600

    Inicio de nuevo curso, creación de paths de auth para el log in de usuario

---
+++ src/modules/auth/routes/index.ts
@@ -0,0 +1,21 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+export const authRoutes: RouteRecordRaw = {
+  path: '/auth',
+  name: 'auth',
+  redirect: 'auth/login',
+  component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+  children: [
+    {
+      path: 'login',
+      name: 'login',
+      component: () => import('@/modules/auth/pages/LoginPage.vue'),
+    },
+
+    {
+      path: 'register',
+      name: 'register',
+      component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+    },
+  ],
+};
